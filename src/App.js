abb4e09e659058a8f89ecd19d4a6c44935a2c06a
Cristian Riffo <cristian.riffo@bairesdev.com>
2020-05-25 15:56:05 +1200

    chaning baseline import order

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Catalog from "./Catalog";
 import Menu from "./Shared/Menu";
-import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
   return (
