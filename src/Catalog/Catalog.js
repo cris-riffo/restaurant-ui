3ccaab9ef2058f5221aa1ee1c7776225f4d364b6
Cristian Riffo <cristian.riffo@bairesdev.com>
2020-05-25 17:33:03 +1200

    small change

--- src/Catalog/Catalog.js
+++ src/Catalog/Catalog.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 
-const Catalog = () => <Grid>Here is gonna be the list of products</Grid>;
+const Catalog = () => <Grid>Here is gonna be the list of products 2</Grid>;
 
 export default Catalog;
