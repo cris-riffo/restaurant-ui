b4695adf1bd16936ceae136441c4a45d4f361490
Cristian Riffo <cristian.riffo@bairesdev.com>
2020-05-25 14:36:24 +1200

    reestructuring app

---
+++ src/Catalog/Catalog.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+
+const Catalog = () => <Grid>Here is gonna be the list of products</Grid>;
+
+export default Catalog;
---
+++ src/Shared/Menu/Menu.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+
+const Menu = () => <Grid>This is the place for the menu</Grid>;
+
+export default Menu;
